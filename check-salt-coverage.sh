2da3f04fd3467b9e33ad72739bf75e8e036acfad
Jeff Tang <jetang@signpost.com>
2016-01-06 11:43:35 -0500

    list both

--- check-salt-coverage.sh
+++ check-salt-coverage.sh
@@ -6,4 +6,8 @@ ssh salt@10.0.17.64 'salt \* grains.get ipv4' | grep '.- 10'|awk '{print $2}' >>
 ssh salt@10.0.17.147 'salt \* grains.get ipv4' | grep '.- 10'|awk '{print $2}' >> $tmpfile
 sort $tmpfile > $salt_ips
 ~/src/ops.awstools/awstools/list-all-ec2-ips.sh| sort > $aws_ips
+echo $(wc -l $aws_ips) AWS ips
+echo Not in salt
 comm -3 -2 $aws_ips $salt_ips
+echo Not in aws
+comm -3 -1 $aws_ips $salt_ips
